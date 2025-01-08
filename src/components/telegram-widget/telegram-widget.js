6057a0ff6cd2fa4af563ea045506e249c3b5a925
ferea <tatamitina19@gmail.com>
2025-01-08 16:37:15 +0300

    tg try 8

--- src/components/telegram-widget/telegram-widget.js
+++ src/components/telegram-widget/telegram-widget.js
@@ -3,27 +3,23 @@ import React, { useEffect } from 'react';
 const TelegramWidget = () => {
   useEffect(() => {
     console.log('Attempting to load Telegram widget...');
-
-    // Создаем тег <script> для Telegram виджета
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
-    script.setAttribute('data-telegram-login', 'photoflex_bot'); // Имя вашего бота
-    script.setAttribute('data-size', 'medium'); // Размер кнопки
-    script.setAttribute('data-onauth', 'onTelegramAuth(user)'); // Callback-функция
-    script.setAttribute('data-request-access', 'write'); // Права доступа
+    script.setAttribute('data-telegram-login', 'photoflex_bot');
+    script.setAttribute('data-size', 'medium');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.setAttribute('data-request-access', 'write');
     script.onload = () =>
       console.log('Telegram widget loaded successfully.');
     script.onerror = (err) =>
       console.error('Failed to load Telegram widget:', err);
 
-    // Добавляем скрипт в контейнер
     const container = document.getElementById(
       'telegram-widget-container'
     );
     container.appendChild(script);
 
-    // Определяем глобальный callback для авторизации
     window.onTelegramAuth = (user) => {
       console.log('Telegram authentication started');
       console.log('User details:', user);
@@ -32,7 +28,6 @@ const TelegramWidget = () => {
         const authToken = user.hash;
         console.log('Auth token received:', authToken);
 
-        // Сохранение токена, пример:
         localStorage.setItem('authToken', authToken);
         console.log('Auth token saved to localStorage');
       } else {
@@ -44,7 +39,6 @@ const TelegramWidget = () => {
     };
 
     return () => {
-      // Чистим контейнер при размонтировании компонента
       if (container) {
         container.innerHTML = '';
       }
@@ -55,9 +49,7 @@ const TelegramWidget = () => {
     <div
       id="telegram-widget-container"
       style={{ textAlign: 'center', marginTop: '20px' }}
-    >
-      {/* Telegram виджет будет загружен в этот div */}
-    </div>
+    ></div>
   );
 };
 
