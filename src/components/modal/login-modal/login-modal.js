9c68966514d17605d39258a599cac2a08a357ec5
ferea <tatamitina19@gmail.com>
2025-01-08 16:44:18 +0300

    tg try 11

--- src/components/modal/login-modal/login-modal.js
+++ src/components/modal/login-modal/login-modal.js
@@ -155,7 +155,7 @@ const LoginModal = ({ onSignUpClick, onSubmited }) => {
               <GoogleIcon />
             </Button>
           </Stack>
-          {/*<TelegramWidget />*/}
+          <TelegramWidget />
 
           <Stack sx={styles.footerStack} direction="row" spacing={1}>
             <span style={styles.footerText}>
--- src/components/telegram-widget/telegram-widget.js
+++ src/components/telegram-widget/telegram-widget.js
@@ -7,10 +7,10 @@ const TelegramWidget = () => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
-    script.setAttribute('data-telegram-login', 'photoflex_bot'); // Имя вашего бота
-    script.setAttribute('data-size', 'medium'); // Размер кнопки
-    script.setAttribute('data-onauth', 'onTelegramAuth(user)'); // Callback-функция
-    script.setAttribute('data-request-access', 'write'); // Права доступа
+    script.setAttribute('data-telegram-login', 'photoflex_bot');
+    script.setAttribute('data-size', 'medium');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    script.setAttribute('data-request-access', 'write');
     script.onload = () =>
       console.log('Telegram widget loaded successfully.');
     script.onerror = (err) =>
@@ -29,7 +29,6 @@ const TelegramWidget = () => {
         const authToken = user.hash;
         console.log('Auth token received:', authToken);
 
-        // Сохранение токена, пример:
         localStorage.setItem('authToken', authToken);
         console.log('Auth token saved to localStorage');
       } else {
@@ -51,8 +50,7 @@ const TelegramWidget = () => {
     <div
       id="telegram-widget-container"
       style={{ textAlign: 'center', marginTop: '20px' }}
-    >
-    </div>
+    ></div>
   );
 };
 
